389dc3c5a64c0759b3076e35e6f79b16208b95b1
KhareV <kharevedant05@gmail.com>
2025-03-10 19:24:39 +0530

    Modified

--- server/server.js
+++ server/server.js
@@ -333,20 +333,6 @@ app.delete("/api/trips/:id", async (req, res) => {
     res.status(500).json({ error: "Failed to delete trip" });
   }
 });
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
-app.post("/create-payment-intent", async (req, res) => {
-  try {
-    const { amount, currency } = req.body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency,
-    });
-
-    res.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
